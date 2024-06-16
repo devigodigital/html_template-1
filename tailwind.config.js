6e6b2359811b8ee248df2af2d9053fd55af95b97
Jacob Ryan <jacob.ryan@copiadigital.com>
2024-06-16 22:05:33 +0100

    feat(package): install aos through npm

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    'src/*',
-    'node_modules/flowbite/**/*.js'
+    './src/*',
+    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
