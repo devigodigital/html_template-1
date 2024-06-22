2e6f9fb283ab0a652b5f9079b79b2e37acbe4bba
Jacob Ryan <jacob.ryan@copiadigital.com>
2024-06-22 19:54:34 +0100

    chore: new blocks

---
+++ src/countup.js
@@ -0,0 +1,25 @@
+$('[data-count]').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+  
+    {
+  
+      duration: 2500,
+      easing:'swing',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+  
+    });  
+    
+    
+  
+  });
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,20 @@ module.exports = {
     extend: {
       fontSize: {
         'h1-desktop': '3.5rem', // 36px
-        'h2-desktop': '1.5rem',  // 24px
+        'h2-desktop': '2.5rem',  // 24px
         'h3-desktop': '2.16rem', // 20px
-        'h4-desktop': '0.83rem',    // 16px
-        'h5-desktop': '0.51rem',// 14px
-        'h6-desktop': '0.31rem', // 12px
+        'h4-desktop': '1.8rem',    // 16px
+        'h5-desktop': '1.2rem', // 14px
+        'h6-desktop': '0.8rem', // 12px
         'p-desktop': '1.2rem',     // 16px
         'ul-desktop': '1.2rem',    // 16px
-        'ol-deskto-': '1.2rem',    // 16px
+        'ol-desktop': '1.2rem',    // 16px
         'li-desktop': '1.2rem',    // 16px
         'h1-mobile': '2.5rem', // 40px
         'h2-mobile': '2rem',  // 32px
         'h3-mobile': '1.75rem', // 28px
-        'h4-mobile': '1.5rem',    // 24px
-        'h5-mobile': '1.25rem',// 20px
+        'h4-mobile': '1.3rem',    // 24px
+        'h5-mobile': '1.2rem',// 20px
         'h6-mobile': '1rem', // 16px
         'p-mobile': '1rem',     // 16px
         'ul-mobile': '1rem',    // 16px
